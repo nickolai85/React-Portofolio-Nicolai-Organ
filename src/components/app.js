71cea38d26f99c221593434a878072245b4b5103
Nicolai <organ.nicolai@gmail.com>
2019-05-02 20:05:27 +0300

    created first component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+
+import PortofolioContainer from './portfolio-container';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>DevCamp React Starter</h1>
-        <h2>Nicolai Organ Portofolio</h2>
+        <PortofolioContainer />
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
 
       </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PortofolioContainer extends Component {
+    render() {
+      return (
+        <div>
+            <h2>Portofolio Items go here ...</h2>
+        </div>
+      );
+    }
+  }
