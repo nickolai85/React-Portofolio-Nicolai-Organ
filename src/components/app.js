4b263d6cc2683b6dd709040f52e342e574a11b81
Nicolai <organ.nicolai@gmail.com>
2019-05-02 20:30:48 +0300

    added functional component in portfolio-container

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import moment from "moment";
 
-import PortofolioContainer from './portfolio-container';
+import PortofolioContainer from './portfolio/portfolio-container';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>DevCamp React Starter</h1>
-        <PortofolioContainer />
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-
+        <PortofolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import PortofolioItem from './portfolio-item';
 export default class PortofolioContainer extends Component {
     render() {
       return (
         <div>
             <h2>Portofolio Items go here ...</h2>
+            <PortofolioItem />
         </div>
       );
     }
