5dd18eaac0790374ff64e23ef489cb935449c609
Nicolai <organ.nicolai@gmail.com>
2019-05-14 22:15:13 +0300

    Using Mixins to Create Button Styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,71 +1,68 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
-import PortofolioItem from './portfolio-item';
-export default class PortofolioContainer extends Component {
-  constructor(){
+import PortfolioItem from './portfolio-item';
+export default class PortfolioContainer extends Component {
+  constructor() {
     super();
     this.state = {
       pageTitle: "Welcome to my Portfolyo!",
       isLoading: false,
-      data: []
-    
-    };
+      data: []    };
 
     this.handleFilter = this.handleFilter.bind(this);
   }
-  getPortfolioItems(){
-    axios.get('https://nick.devcamp.space/portfolio/portfolio_items')
-    .then(response => {
-    
-      this.setState({
-        data : response.data.portfolio_items
-      });
 
-    })
-    .catch(error => {
-      console.log(error);
-    });
-
-  }
-  portofolioItems(){
-    return this.state.data.map(item =>{
-      return(
-         <PortofolioItem 
-            key={item.id}
-            item={item}
-         />
-      );
-    });
-  }
-
-  
-  handleFilter(filter){
+  handleFilter(filter) {
     this.setState({
-      data: this.state.data.filter(item =>{
+      data: this.state.data.filter(item => {
         return item.category === filter;
-      } )
-    })
+      })
+    });
   }
-  componentDidMount(){
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
     this.getPortfolioItems();
   }
-    render() {
-      if(this.state.isLoading){
-        return <div>Loading ...</div>
-      }
-      return (
-        <div>
-            <h2>{this.state.pageTitle}</h2>
-            
-            <button onClick = {() => this.handleFilter('HVAC')}>HVAC</button>
-            <button onClick = {() => this.handleFilter('IT')}>IT</button>
-            <button onClick = {() => this.handleFilter('eComerce')}>eComerce</button>
 
-            <div className="portfolio-items-wrapper">
-            {this.portofolioItems()}
-            </div>
-        </div>
-      );
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
+
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter('HVAC')}>
+           HVAC
+        </button>
+        <button className="btn" onClick={() => this.handleFilter('IT')}>
+           IT
+        </button>
+        <button className="btn" onClick={() => this.handleFilter('eComerce')}>
+          eCommerce
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
   }
+}
