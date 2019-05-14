b030ee9b62cbbd393426ae94ea44438d6ee89a0e
Nicolai <organ.nicolai@gmail.com>
2019-05-14 22:19:59 +0300

    Authentification component

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match"
 export default class App extends Component {
 
@@ -26,6 +27,7 @@ export default class App extends Component {
             <NavigationContainer />
             <Switch>
               <Route exact path = "/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path = "/about-me" component={About} />
               <Route path = "/contact" component={Contact} />
               <Route path = "/blog" component={Blog} />
