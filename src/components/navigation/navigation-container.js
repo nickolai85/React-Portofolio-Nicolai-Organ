afd4354e0115ae066794d0dac35d27583e50aae0
Nicolai <organ.nicolai@gmail.com>
2019-05-27 23:07:56 +0300

    Font Awesome Naming Requirements for React Components

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
@@ -59,6 +60,9 @@ const NavigationComponent = props => {
 
       <div className="right-side">
         NICOLAI ORGAN
+        <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt" />
+          </a>
         {props.loggedInStatus === "LOGGED_IN" ? (<a onClick={handleSignOut}>Sign Out</a>) : null}
       </div>
     </div>
