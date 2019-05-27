a6498c23fda2b1a0ca1bb50dbde5f24edcbaf215
Nicolai <organ.nicolai@gmail.com>
2019-05-27 22:59:57 +0300

    Installing and Configuring Font Awesome in React

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,9 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from "./pages/home";
@@ -17,6 +19,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match"
 import axios from "axios";
+library.add(faTrash, faSignOutAlt);
 
 export default class App extends Component {
   constructor(props) {
