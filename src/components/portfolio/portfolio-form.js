3e92a322e560d34a742ac319895ada4e9099f15a
Nicolai Organ <nicolaiorgan@Nicolais-MacBook-Pro.local>
2019-05-28 17:02:50 +0300

    Dynamic Axios API Queries

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -17,7 +17,10 @@ export default class PortfolioForm extends Component {
           url: "",
           thumb_image: "",
           banner_image: "",
-          logo: ""
+          logo: "",
+          editMode: false,
+          apiUrl: "https://nick.devcamp.space/portfolio/portfolio_items",
+          apiAction: "post"
         };
     
         this.handleChange = this.handleChange.bind(this);
@@ -53,7 +56,10 @@ export default class PortfolioForm extends Component {
             description: description || "",
             category: category || "eCommerce",
             position: position || "",
-            url: url || ""
+            url: url || "",
+            editMode: true,
+            apiUrl: `https://nick.devcamp.space/portfolio/portfolio_items/${id}`,
+            apiAction: "patch"
           });
         }
       }
@@ -117,11 +123,12 @@ export default class PortfolioForm extends Component {
       
         handleSubmit(event) {
           axios
-          .post(
-            "https://nick.devcamp.space/portfolio/portfolio_items",
-            this.buildForm(),
-            { withCredentials: true }
-          )
+          axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+          })
           .then(response => {
             this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
             this.setState({
