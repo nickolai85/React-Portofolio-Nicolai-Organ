ca36161f2758bf4923a6598460dfe9dd7a3e0bf3
Nicolai <organ.nicolai@gmail.com>
2019-05-28 23:37:13 +0300

    Converting the Functional Blog Component into a Class Component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function(props){
-   return(
-       <div>
-          <h2> Blog </h2>
-       <div>
-            <Link to="/about-me">Read more about myself </Link>
+class Blog extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about myself</Link>
         </div>
-       </div>
-    )
+      </div>
+    );
+  }
 }
+
+export default Blog;
