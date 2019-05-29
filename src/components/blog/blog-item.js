c4b3d31479223bb30f1eaa68c367e9177e263538
Nicolai <organ.nicolai@gmail.com>
2019-05-29 09:19:38 +0300

    Building the Initial Blog Detail Component

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const BlogItem = props => {
   const {
     id,
@@ -11,7 +11,9 @@ const BlogItem = props => {
 
   return (
     <div>
-      <h1>{title}</h1>
+      <Link to={`/b/${id}`}>
+        <h1>{title}</h1>
+      </Link>
       <div>{content}</div>
     </div>
   );
