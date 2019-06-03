c0d7a6ceed313826fca9df56d26b9189426a01f3
Nicolai <organ.nicolai@gmail.com>
2019-06-03 23:10:14 +0300

    Building a Dedicated Icon Helper File in React

--- src/components/app.js
+++ src/components/app.js
@@ -5,15 +5,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faSignOutAlt,
-  faEdit,
-  faSpinner,
-  faPlusCircle
-} from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from "./pages/home";
@@ -26,12 +19,12 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match"
 import axios from "axios";
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner,faPlusCircle);
+import Icons from "../helpers/icons";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
+    Icons();
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
     };
