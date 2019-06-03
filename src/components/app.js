cdb2ba3a3c0e5d2c4aa2820b835cf722a0a92317
Nicolai <organ.nicolai@gmail.com>
2019-06-03 22:07:33 +0300

    Building the Styles for the New Blog Icon with a Fixed Position on the Page

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,8 @@ import {
   faTrash,
   faSignOutAlt,
   faEdit,
-  faSpinner
+  faSpinner,
+  faPlusCircle
 } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from './navigation/navigation-container';
@@ -25,7 +26,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match"
 import axios from "axios";
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner,faPlusCircle);
 
 export default class App extends Component {
   constructor(props) {
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -103,7 +103,9 @@ class Blog extends Component {
           modalIsOpen={this.state.blogModalIsOpen}
         />
           <div className="new-blog-link">
-            <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+          <a onClick={this.handleNewBlogClick}>
+            <FontAwesomeIcon icon="plus-circle" />
+          </a>
           </div>
           <div className="content-container">{blogRecords}</div>
             {this.state.isLoading ? (
