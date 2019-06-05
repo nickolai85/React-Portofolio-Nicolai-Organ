cbdb3fd28496850c221a4a79ccb2a05fa4092eb1
Nicolai <organ.nicolai@gmail.com>
2019-06-05 22:56:40 +0300

    Adding Click Handler to Render the BlogForm Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -2,56 +2,70 @@ import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
+
 export default class BlogDetail extends Component {
-    constructor(props) {
-        //calling props from the route
-        super(props);
-    
-        this.state = {
-            //grab id from url
-          currentId: this.props.match.params.slug,
-          blogItem: {}
-        };
-      }
-    
-      getBlogItem() {
-        axios
-          .get(
-            `https://nick.devcamp.space/portfolio/portfolio_blogs/${
-              this.state.currentId}`
-          )
-          .then(response => {
-            this.setState({
-                blogItem: response.data.portfolio_blog
-              });
-          })
-          .catch(error => {
-            console.log("getBlogItem error", error);
-          });
-      }
-    
-      componentDidMount() {
-        this.getBlogItem();
-      }
-    
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+      editMode: false
+    };
+
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick() {
+    console.log("handle edit clicked");
+    this.setState({ editMode: true });
+  }
+
+  getBlogItem() {
+    axios
+      .get(
+        `https://nick.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId }`
+      )
+      .then(response => {
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
   render() {
     const {
-        title,
-        content,
-        featured_image_url,
-        blog_status
-      } = this.state.blogItem;
-    return (
-        <div className="blog-container">
-        <div className="content-container">
-          <h1>{title}</h1>
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
 
-          <BlogFeaturedImage img={featured_image_url} />
+    const contentManager = () => {
+      if (this.state.editMode) {
+        return <BlogForm />;
+      } else {
+        return (
+          <div className="content-container">
+            <h1 onClick={this.handleEditClick}>{title}</h1>
 
-          <div className="content">{ReactHtmlParser(content)}</div>
-        </div>
-      </div>
-    );
+            <BlogFeaturedImage img={featured_image_url} />
+
+            <div className="content">{ReactHtmlParser(content)}</div>
+          </div>
+        );
+      }
+    };
+
+    return <div className="blog-container">{contentManager()}</div>;
   }
 }
