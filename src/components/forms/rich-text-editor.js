8fde9327ba124183ad45d34a246899e115576d2e
Nicolai <organ.nicolai@gmail.com>
2019-06-05 22:37:41 +0300

    Converting Image Files into Base64 Strings

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -3,7 +3,8 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
-
+import { resolve } from "path";
+import { rejects } from "assert";
 export default class RichTextEditor extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +13,8 @@ export default class RichTextEditor extends Component {
       editorState: EditorState.createEmpty()
     };
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);   
   }
   onEditorStateChange(editorState) {
     this.setState(
@@ -23,8 +26,16 @@ export default class RichTextEditor extends Component {
       )
     );
   }
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = error => {};
+  }
   uploadFile(file) {
-    console.log("upload file", file);
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, data => resolve({ data: { link: data } }));
+    });
   }
   render() {
     return (
