8547ef0d22587df60c9e7abe2b9ec8a20d1f5169
Nicolai <organ.nicolai@gmail.com>
2019-06-11 22:59:04 +0300

    Full Filtering Feature Build Out in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -14,20 +14,28 @@ export default class PortfolioContainer extends Component {
   }
 
   handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.category === filter;
-      })
-    });
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
   }
 
-  getPortfolioItems() {
+  getPortfolioItems(filter = null) {
     axios
       .get("https://nick.devcamp.space/portfolio/portfolio_items")
       .then(response => {
-        this.setState({
-          data: response.data.portfolio_items
-        });
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter(item => {
+              return item.category === filter;
+            })
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items
+          });
+        }
       })
       .catch(error => {
         console.log(error);
@@ -50,21 +58,36 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter('HVAC')}>
-           HVAC
-        </button>
-        <button className="btn" onClick={() => this.handleFilter('IT')}>
-           IT
-        </button>
-        <button className="btn" onClick={() => this.handleFilter('eComerce')}>
-          eCommerce
-        </button>
-        <button className="btn" onClick={() => this.handleFilter('retail')}>
-          Retail
-        </button>
-        {this.portfolioItems()}
-      </div>
+
+<div className="homepage-wrapper">
+<div className="filter-links">
+  <button
+    className="btn"
+    onClick={() => this.handleFilter("HVAC")}
+  >
+    HVAC
+  </button>
+  <button
+    className="btn"
+    onClick={() => this.handleFilter("IT")}
+  >
+    IT
+  </button>
+  <button
+    className="btn"
+    onClick={() => this.handleFilter("eComerce")}
+  >
+    eCommerce
+  </button>
+  <button
+    className="btn"
+    onClick={() => this.handleFilter("CLEAR_FILTERS")}
+  >
+    All
+  </button>
+</div>
+<div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+</div>
     );
   }
 }
